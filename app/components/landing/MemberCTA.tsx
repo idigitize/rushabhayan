4f302545722b4dfc56a249037a3c2816dcb73bd9
PiyushKalyanpy <kalyanpiyushp@gmail.com>
2026-03-06 18:35:14 +0530

    bug fixed

--- app/components/landing/MemberCTA.tsx
+++ app/components/landing/MemberCTA.tsx
@@ -4,17 +4,19 @@ import Button from "../ui/Button";
 
 const MemberCTA = () => {
   return (
-    <section className="relative hidden md:flex  w-full py-20  px-4 md:px-8 ">
+    <section className="relative flex flex-row md:flex-col w-full py-20  md:px-4 md:px-8 ">
       <div className=" w-full bg-[#5E2A16] text-[#f3e5d0] p-10 md:p-16 flex flex-col md:flex-row items-center justify-between gap-8 shadow-2xl relative overflow-hidden">
         <div className="flex flex-col gap-2 z-10 text-center md:text-left">
-          <h2 className="text-[32px]  font-semibold font-manrope">
+          <h2 className="text-[24px] md:text-[32px]  font-semibold font-manrope">
             Join Us At Rushabhayan 3
           </h2>
-          <p className="text-[56px] font-didot italic">Register Now!</p>
+          <p className="text-[48px] md:text-[56px] font-didot italic">
+            Register Now!
+          </p>
         </div>
 
         <div className="z-10">
-          <div className="hidden md:block pl-4">
+          <div className="block pl-4">
             <Button
               variant="primary"
               size="md"
